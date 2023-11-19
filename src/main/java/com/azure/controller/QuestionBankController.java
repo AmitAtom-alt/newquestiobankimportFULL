9b927d477f603495d78462e0cf63834ab386d3f6
Amit <atomamit622@gmail.com>
2023-11-19 18:34:43 +0530

    2nd updated

--- src/main/java/com/azure/controller/QuestionBankController.java
+++ src/main/java/com/azure/controller/QuestionBankController.java
@@ -1,7 +1,11 @@
 package com.azure.controller;
 
 
+import com.azure.entity.Questionnaire;
+import com.azure.entity.qBank.QuestionBank;
+import com.azure.exception.QuestionnaireException;
 import com.azure.payload.qBank.QuestionBankDTO;
+import com.azure.repository.qBank.QuestionBankRepo;
 import com.azure.service.QuestionBankService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,15 +15,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class QuestionBankController {
     @Autowired
     private QuestionBankService questionBankService;
+    @Autowired
+    private QuestionBankRepo questionBankRepo;
 
     @PostMapping("/import")
     public ResponseEntity<QuestionBankDTO> importQuestionBank(@RequestBody QuestionBankDTO questionBankDTO){
 
+        QuestionBank existingQuestionBank = questionBankRepo.findByBankCode(questionBankDTO.getBankCode());
+        if (existingQuestionBank != null){
+            throw new QuestionnaireException("QB with name: "+questionBankDTO.getBankCode()+" already exists");
+        }
         QuestionBankDTO questionBankDTO1 = questionBankService.importQuestionBank(questionBankDTO);
         return new ResponseEntity<>(questionBankDTO, HttpStatus.CREATED);
 
--- src/main/java/com/azure/repository/qBank/QuestionBankRepo.java
+++ src/main/java/com/azure/repository/qBank/QuestionBankRepo.java
@@ -3,6 +3,13 @@ package com.azure.repository.qBank;
 
 import com.azure.entity.qBank.QuestionBank;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+@Repository
 public interface QuestionBankRepo extends JpaRepository<QuestionBank, Long> {
+    @Query("SELECT qb FROM QuestionBank qb WHERE qb.bankCode = :bankCode")
+    QuestionBank findByBankCode(@Param("bankCode") String bankCode);
 }
